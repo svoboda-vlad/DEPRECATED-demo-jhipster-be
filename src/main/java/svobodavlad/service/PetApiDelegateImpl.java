24a5fe584cb0940c3f06fa278b1a2680033c927e
Vladimír Svoboda <svobo@SVOBODAITNB.koposko.local>
2022-08-14 16:21:31 +0200

    openapi model DTO suffix

--- src/main/java/svobodavlad/service/PetApiDelegateImpl.java
+++ src/main/java/svobodavlad/service/PetApiDelegateImpl.java
@@ -6,18 +6,18 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import svobodavlad.service.api.dto.Pet;
+import svobodavlad.service.api.dto.PetDTO;
 import svobodavlad.web.api.PetsApiDelegate;
 
 @Service
 public class PetApiDelegateImpl implements PetsApiDelegate {
 
     @Override
-    public ResponseEntity<List<Pet>> listPets(Integer limit) {
-    	var pet = new Pet();
-    	pet.setId(123L);
-    	pet.setName("my pet");
-    	var petList = new ArrayList<Pet>(List.of(pet));
+    public ResponseEntity<List<PetDTO>> listPets(Integer limit) {
+    	var petDTO = new PetDTO();
+    	petDTO.setId(123L);
+    	petDTO.setName("my pet");
+    	var petList = new ArrayList<PetDTO>(List.of(petDTO));
         return ResponseEntity.ok().body(petList);
 
     }
